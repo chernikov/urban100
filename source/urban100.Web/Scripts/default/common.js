bdadb8e3ebff9cdc2561032ef1ba27e56f77a86e
Andrey Chernikov <chernikov@gmail.com>
2014-07-01 15:35:08 +0300

    Revert "Fix html"
    This reverts commit 8da599cf423414712fd552f20ab2a5cbc3e2ba2a.

---
+++ source/urban100.Web/Scripts/default/common.js
@@ -0,0 +1,17 @@
+﻿function Common() {
+    var _this = this;
+
+    this.init = function () {
+        $('.dropdown-toggle').dropdown();
+
+        $('.delete-action').live('click', function () {
+            return confirm("Действительно удалять?");
+        });
+    };
+}
+
+var common = null;
+$(function () {
+    common = new Common();
+    common.init();
+});
